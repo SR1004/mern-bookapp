f5b70894afc1f980023e5005e0b25358428dc550
SR1004 <sriramsg4@gmail.com>
2025-08-13 23:08:43 +0530

    Initial commit of MERN book app

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { PORT,mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js"
+import cors from 'cors';
+
+const app=express();
+
+//parse req
+app.use(express.json());
+
+//middleware
+//opt 1
+app.use(cors());
+//opt 2
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET','POST','PUT','DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// )
+
+app.get('/', (request,response) => {
+    console.log(request)
+    return response.status(234).send("Works")
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() =>{
+        console.log('App logged in');
+        app.listen(PORT, () => {
+            console.log(`App is listening to: ${PORT}`);
+        });
+    })
+    .catch((error) =>{
+        console.log(error);
+    });
