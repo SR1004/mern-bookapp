da4b6242bd50681015c929591a55609244cd7efb
SR1004 <120243957+SR1004@users.noreply.github.com>
2025-08-13 23:13:04 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555; 
 
-export const mongoDBURL = 'mongodb+srv://sriramsg4:cVH9yIuUGjQOgrpB@book-store.bafri33.mongodb.net/books?retryWrites=true&w=majority&appName=Book-store'
+export const mongoDBURL = //MongoDB URL with Password
